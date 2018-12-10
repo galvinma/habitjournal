4fcab382d6cdeddbf8a0690650dde9b2b5a831b1
Matthew Galvin <mattgalvin47@gmail.com>
2018-12-10 10:34:54 -0800

    final mongo uri update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ cors({credentials: true, origin: true})
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-mongoose.connect('localhost:27017/database', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/database', { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
