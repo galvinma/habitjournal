76c5d6d046e6843140879336896958e9a037c405
Matthew Galvin <mattgalvin47@gmail.com>
2018-12-19 16:47:49 -0800

    update git ignore to include build

---
+++ build/precache-manifest.61c347469567fbc5c07b404cdc320d63.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "807cf02d471ccfa6e2f8001e14b7c51e",
+    "url": "/static/media/flower.807cf02d.svg"
+  },
+  {
+    "revision": "0c7e6c7d8163744f0b51",
+    "url": "/static/css/main.0b2329f6.chunk.css"
+  },
+  {
+    "revision": "b91bdf1b3fc2124f338895fdb6b121be",
+    "url": "/static/media/plus.b91bdf1b.svg"
+  },
+  {
+    "revision": "9b078bf008eb1dbe5e24",
+    "url": "/static/js/1.9b078bf0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f039223a002527ddd547e37bbcb1a8d",
+    "url": "/static/media/minus.4f039223.svg"
+  },
+  {
+    "revision": "707ab4efdc3f71a8714d8a8271d37a63",
+    "url": "/static/media/checkbox-blank-circle-outline.707ab4ef.svg"
+  },
+  {
+    "revision": "9baa5a0f76cfb1fce9e28c78c86cbba8",
+    "url": "/static/media/checkbox-blank-outline.9baa5a0f.svg"
+  },
+  {
+    "revision": "522143dbb735e93f2abdb939c8d25008",
+    "url": "/static/media/checkbox-blank-triangle-outline.522143db.svg"
+  },
+  {
+    "revision": "e5869fb4bfca4cf3dabe36446623af76",
+    "url": "/static/media/checkbox-blank-circle.e5869fb4.svg"
+  },
+  {
+    "revision": "88cf3d969de39735fb233c4e61046dd3",
+    "url": "/static/media/checkbox-blank-triangle.88cf3d96.svg"
+  },
+  {
+    "revision": "3d61455c656744edf3e55db840131868",
+    "url": "/static/media/checkbox-blank.3d61455c.svg"
+  },
+  {
+    "revision": "3e191ab0b13d8c29aa8a4d541a4633e2",
+    "url": "/static/media/checkbox-multiple-blank-circle-outline.3e191ab0.svg"
+  },
+  {
+    "revision": "602d499a9fba36af4be65aa3e3b388a8",
+    "url": "/static/media/checkbox-multiple-blank-circle.602d499a.svg"
+  },
+  {
+    "revision": "0c650ff7cbc3b02f6ffe1d8f8b3111d1",
+    "url": "/static/media/checkbox-multiple-blank-outline.0c650ff7.svg"
+  },
+  {
+    "revision": "e21e13e8686e80dcaa82b3a8fb63576a",
+    "url": "/static/media/checkbox-multiple-blank-triangle-outline.e21e13e8.svg"
+  },
+  {
+    "revision": "523aad74c6b1769b48ec650a926e52ec",
+    "url": "/static/media/checkbox-multiple-blank-triangle.523aad74.svg"
+  },
+  {
+    "revision": "8fe078d9ebc03f9b0c5b276c493fc3a8",
+    "url": "/static/media/checkbox-multiple-blank.8fe078d9.svg"
+  },
+  {
+    "revision": "d80a45f03d5372246aa247088109a4d7",
+    "url": "/static/media/flower-outline.d80a45f0.svg"
+  },
+  {
+    "revision": "0c7e6c7d8163744f0b51",
+    "url": "/static/js/main.0c7e6c7d.chunk.js"
+  },
+  {
+    "revision": "15381de6b42ba0bcf3c5fe9d4eb080e9",
+    "url": "/static/media/logo.15381de6.svg"
+  },
+  {
+    "revision": "fe7bfab27044b56d84ba93c669f8ba41",
+    "url": "/static/media/prompt.fe7bfab2.svg"
+  },
+  {
+    "revision": "bb3fd42f77784546e8181b8b806306da",
+    "url": "/static/media/weather-night.bb3fd42f.svg"
+  },
+  {
+    "revision": "df3ff3807f4079f24e14748eb02ed0f3",
+    "url": "/static/media/weather-sunset.df3ff380.svg"
+  },
+  {
+    "revision": "00cf30735e584970b1ef490cb921392d",
+    "url": "/static/media/key.00cf3073.svg"
+  },
+  {
+    "revision": "a1a04ec91b0e9801948065a8e0283bf2",
+    "url": "/static/media/archive.a1a04ec9.svg"
+  },
+  {
+    "revision": "bf20a07752f6318fde8980ac260ebec3",
+    "url": "/static/media/dots-horizontal.bf20a077.svg"
+  },
+  {
+    "revision": "32d19c01b96dfdf46d1b1f0a582320ff",
+    "url": "/static/media/close.32d19c01.svg"
+  },
+  {
+    "revision": "c1074e00554b04313eccd369ccc4a069",
+    "url": "/static/media/dots-vertical.c1074e00.svg"
+  },
+  {
+    "revision": "a944cbede18847e0479c7337b4fcdc64",
+    "url": "/static/media/pencil-outline.a944cbed.svg"
+  },
+  {
+    "revision": "16a9a3af8c1b17375cdfe21c907885c7",
+    "url": "/static/media/landing.16a9a3af.svg"
+  },
+  {
+    "revision": "710d474ab5acf92fef20d929cf73a902",
+    "url": "/static/media/about.710d474a.svg"
+  },
+  {
+    "revision": "9777a63c9e5c9ef6fe6823c729b2512e",
+    "url": "/static/media/404.9777a63c.svg"
+  },
+  {
+    "revision": "8e311d15e3dd7966841f68b8dde2d7ea",
+    "url": "/static/media/journal.8e311d15.png"
+  },
+  {
+    "revision": "588f0a067b09e17d7cd0eb76ee024f0f",
+    "url": "/static/media/habits.588f0a06.png"
+  },
+  {
+    "revision": "bcc928ed274e1c3909a2de1c65489bcf",
+    "url": "/static/media/calendar.bcc928ed.png"
+  },
+  {
+    "revision": "9b078bf008eb1dbe5e24",
+    "url": "/static/css/1.78da188b.chunk.css"
+  },
+  {
+    "revision": "aa19390618c65a2b893e4ebd9996a302",
+    "url": "/index.html"
+  }
+];
