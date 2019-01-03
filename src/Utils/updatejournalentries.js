b2009c3e04062b5828e55ea91e77c039aca57a8f
Matthew Galvin <mattgalvin47@gmail.com>
2019-01-02 16:44:57 -0800

    more journal logging

--- src/Utils/updatejournalentries.js
+++ src/Utils/updatejournalentries.js
@@ -1,3 +1,8 @@
+// redux
+import store from '.././Store/store'
+import { connect } from "react-redux";
+import { getAllEntries } from '.././Actions/actions'
+
 export function updateJournalEntries()
 {
   return new Promise((resolve, reject) => {
