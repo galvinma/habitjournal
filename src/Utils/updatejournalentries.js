790c3dd6377de19e510be2dd0d8ee5a7774ca3c5
Matthew Galvin <mattgalvin47@gmail.com>
2019-01-02 16:46:44 -0800

    console typo

--- src/Utils/updatejournalentries.js
+++ src/Utils/updatejournalentries.js
@@ -8,7 +8,7 @@ export function updateJournalEntries()
   return new Promise((resolve, reject) => {
     this.returnAllDatabaseEntries()
     .then((response) => {
-        console.log(response.date.entries)
+        console.log(response.data.entries)
         console.log(store.getState().all_entries.all_entries)
         this.getBullets()
         resolve()
