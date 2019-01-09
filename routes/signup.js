4fa47af618964d372f929a93690385c134b35ffd
Matthew Galvin <mattgalvin47@gmail.com>
2019-01-09 00:02:32 -0800

    update join logic

--- routes/signup.js
+++ routes/signup.js
@@ -30,32 +30,34 @@ router.route('/signup')
       });
       return
     }
+    else
+    {
+      var signup_user = new Users();
+      signup_user.id = new ObjectId();
+      signup_user.firstname = req.body.params.firstname;
+      signup_user.lastname = req.body.params.lastname;
+      signup_user.email = req.body.params.email;
+      signup_user.password = req.body.params.password;
+      signup_user.join_date = moment().unix();
+      signup_user.reset_count = 0;
 
-    var signup_user = new Users();
-    signup_user.id = new ObjectId();
-    signup_user.firstname = req.body.params.firstname;
-    signup_user.lastname = req.body.params.lastname;
-    signup_user.email = req.body.params.email;
-    signup_user.password = req.body.params.password;
-    signup_user.join_date = moment().unix();
-    signup_user.reset_count = 0;
-
-    signup_user.save(function(err) {
-        if (err)
-        {
-          console.log(err)
-        }
-        else
-        {
-          // create token
-          var token = generateJWT.generateJWT(signup_user)
-          res.json({
-            allow: true,
-            user: signup_user.id,
-            token: token,
-          });
-        }
-    });
+      signup_user.save(function(err) {
+          if (err)
+          {
+            console.log(err)
+          }
+          else
+          {
+            // create token
+            var token = generateJWT.generateJWT(signup_user)
+            res.json({
+              allow: true,
+              user: signup_user.id,
+              token: token,
+            });
+          }
+      });  
+    }
   })
 });
 
